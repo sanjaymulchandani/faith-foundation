a4d5b18b74ff272ceafb8e93f5c2763b0c64db63
sanjaymulchandani <sunnymulchandani07@gmail.com>
2022-11-28 23:55:33 +0530

    add changes

--- main/main/settings.py
+++ main/main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vitrhi0842af%txw^$2=92tm^9q3t(dfx=k_$wsju&iziz^z5g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,12 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
