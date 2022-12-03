eedf70d708185215b7a1bca613a7754faf681cca
sanjaymulchandani <sunnymulchandani07@gmail.com>
2022-12-03 20:44:32 +0530

    login/register ui

--- usermanagement/admin.py
+++ usermanagement/admin.py
@@ -1,4 +1,2 @@
 from django.contrib import admin
-from .models import SpiritualCamp
 # Register your models here.
-admin.site.register(SpiritualCamp)
--- usermanagement/views.py
+++ usermanagement/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .forms import SpiritualCampForm
 
 # Create your views here. ONLY LOGIN/REGISTER/LOGOUT/ACCOUNT HANDLING
 def register(request):
     return render(request, 'register.html')
 
+def login(request):
+    return render(request, 'login.html')
 
